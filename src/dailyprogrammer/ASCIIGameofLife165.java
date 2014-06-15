812a34b42f4d927967dd65e8fc8c9b8fcba17db6
Ekrazit <x3918410@gmail.com>
2014-06-15 21:38:38 +0200

    ASCII Game of Life 165

--- src/dailyprogrammer/ASCIIGameofLife165.java
+++ src/dailyprogrammer/ASCIIGameofLife165.java
@@ -1,3 +1,8 @@
+/*	 ASCII Game of Life
+ * 
+ * 
+ */
+
 package dailyprogrammer;
 
 import java.util.Scanner;
@@ -5,27 +10,53 @@ import java.util.Scanner;
 public class ASCIIGameofLife165 {
 	
 	private String[][] matrix;
-	private int iretations = 0;
+	private int iretations = 1;
 
 	public ASCIIGameofLife165() {
 		init();
-		step(matrix);
+		for (int i = 0; i < iretations; i++) {
+			this.matrix = step(matrix);	
+		} 
+		 
 		printMatrix(matrix);
 	}
 
-	private void step(String[][] matrix2) {
-		for (int row = 0; row < matrix2.length; row++) {
-			for (int col = 0; col < matrix2[0].length; col++) {
+	private String[][] step(String[][] matrix2) {		
+		
+		for (int row = 0; row < matrix2[0].length; row++) {
+			for (int col = 0; col < matrix2.length; col++) {
 				
+				int activecells = 0;
+				for (int comprow = -1; comprow < 2; comprow++){
+					for (int compcol = -1; compcol < 2; compcol++) {
+						int temprow;
+						if (row + comprow < 0) temprow = matrix2[0].length - 1;
+						else temprow = row - comprow; 
+						int tempcol;
+						if (col + compcol < 0) tempcol = matrix2.length -1;
+						else tempcol = col - compcol; 
+						if(matrix2[temprow][tempcol].equals("#"))
+							activecells++;
+						
+					}
+				}
+					
+				if (activecells < 2) matrix2[row][col] = ".";
+				if (activecells == 3) matrix2[row][col] = "#";
+				if (activecells > 3) matrix2[row][col] = ".";
+			
 			}
+		
 		}
+
+		return matrix2;
 		
 	}
 
 	private void printMatrix(String[][] matrix2) {
 		for (int row = 0; row < matrix2.length; row++) {
 			for (int col = 0; col < matrix2[0].length; col++) {
-				System.out.println(matrix[row][col]);
+				System.out.print(matrix[row][col]);
 			}
 			System.out.println(" ");
 		}
@@ -40,7 +71,7 @@ public class ASCIIGameofLife165 {
 		matrix = new String[maticaX][maticaY];
 
 		for (int i = 0; i < maticaX; i++) {
-			matrix[i] = scan.nextLine().split("\\s+");
+			matrix[i] = scan.nextLine().split("(?!^)");
 		}
 		
 		
